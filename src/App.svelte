08a28d4231489330afa2e990cd75a4ff0ecc2867
Onder Bakirtas <onderbakirtas@gmail.com>
2021-02-18 06:13:24 +0300

    add: container mixin

--- src/App.svelte
+++ src/App.svelte
@@ -5,14 +5,21 @@
 
 <Navbar />
 
-<Route path="/">Home</Route>
+<Route path="/">
+  <div class="view">Home</div>
+</Route>
 <Route path="/profile">Profile</Route>
 <Route path="/settings">Settings</Route>
 
 <style lang="scss">
   @import 'styles/colors';
+  @import 'styles/mixins';
 
   :global(body) {
     background: $gray-200;
+  }
+
+  .view {
+    @include container;
   }
 </style>
