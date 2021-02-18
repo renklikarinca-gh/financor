512577f8b2588d7eef87b55539632ec494649eb1
Onder Bakirtas <onderbakirtas@gmail.com>
2021-02-18 04:37:18 +0300

    fix: version

---
+++ src/componets/Navbar.svelte
@@ -0,0 +1,14 @@
+<nav class="nav">
+  <a href="/">Home</a>
+  <a href="/profile">Profile</a>
+  <a href="/settings">Settings</a>
+</nav>
+
+<style lang="scss">
+  @import '../styles/colors';
+  
+  .nav {
+    background: $gray-50;
+    padding: 1rem;
+  }
+</style>
